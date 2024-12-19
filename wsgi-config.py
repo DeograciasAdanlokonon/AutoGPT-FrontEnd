f1eb7b17954865fa280fa526cfcd69ac00bdbe4a
DeograciasAdanlokonon <adanlokonondeogracias1@gmail.com>
2024-12-19 10:48:13 +0100

    second commit

--- wsgi-config.py
+++ wsgi-config.py
@@ -9,5 +9,5 @@ if path not in sys.path:
 from app import app as application  # noqa
 
 # Configuration des variables d'environnement
-os.environ['OPENAI_API_KEY'] = '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'
-os.environ['SECRET_KEY'] = 'zjEJhe69566kjsgsFZkdjhdl@ejire5F£skjsd8qf^{YREJIHETYURSBHSQ654'
+os.environ['OPENAI_API_KEY'] = 'your_key'
+os.environ['SECRET_KEY'] = 'your_secret_key'
